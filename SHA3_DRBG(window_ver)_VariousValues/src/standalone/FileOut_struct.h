6349a2915b8eff4aa1994937c217c2e7a031cf86
kyu-h <tigerk9212@gmail.com>
2018-06-07 17:58:16 +0900

    Dynamic allocation.. & various system values

--- SHA3_DRBG(window_ver)_VariousValues/src/standalone/FileOut_struct.h
+++ SHA3_DRBG(window_ver)_VariousValues/src/standalone/FileOut_struct.h
@@ -3,27 +3,7 @@
 typedef unsigned char BitSequence;
 
 struct DRBG_SHA3 {
-	BitSequence dfInput[1000];
-	BitSequence dfOutput[1000];
-	unsigned int Vlen;
-	BitSequence dfInput01[1000];
-	BitSequence dfOutput01[1000];
-	unsigned int reseed_counter;
-	BitSequence addInput[1000];
-	unsigned int addInput_length;
-	BitSequence W_VaddInput[1000];
-	unsigned int W_VaddInput_length;
-	BitSequence V_Mod[1000];
-	unsigned int V_Mod_length;
-	BitSequence Output01[1000];
-	unsigned int Output01_length;
-	BitSequence W_03V[1000];
-	unsigned int W_03V_length;
-	BitSequence V_wCreseed[1000];
-	unsigned int V_wCreseed_03V_length;
-
-	BitSequence V_secondcall[1000];
-	unsigned int V_secondcall_length;
+	unsigned int rate;
 
 	FILE *file_output;
 };
