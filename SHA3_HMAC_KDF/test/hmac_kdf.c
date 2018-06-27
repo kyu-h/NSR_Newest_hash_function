699b87c9fe74d5fab3153bb2c4acc9d0b4f49e37
kyu-h <tigerk9212@gmail.com>
2018-06-27 14:11:22 +0900

    fb is done

--- SHA3_HMAC_KDF/test/hmac_kdf.c
+++ SHA3_HMAC_KDF/test/hmac_kdf.c
@@ -11,22 +11,24 @@ void kdf_sha3_hmac_dp(const unsigned int rate, const unsigned int capacity, cons
 
 void kdf_sha3_hmac_fb(const unsigned int rate, const unsigned int capacity, const unsigned char delimitedSuffix, BitSequence *Kl, unsigned int Kl_len, BitSequence *Label, const unsigned int Label_len, BitSequence *Context, unsigned int Context_len, unsigned int L_len, unsigned int h_len, const unsigned int r, FILE *outf){
 	unsigned int n = 0;
-	int input_data_len = 0;
-	BitSequence result[128] = {'\0', }; //고쳐야함
 	BitSequence output[128];
 	int w = 0;
 	int num = 0;
-	int L_len_to_Hex = L_len * 8;
+	int L_len_to_Hex = L_len;
 	int output_len = 0;
-
-	input_data_len = Kl_len + Label_len + Context_len + 3;
-
+	int input_data_len = Label_len + Context_len + 4;
 	BitSequence input_data[128] = {'\0', };
+	BitSequence tmp_input_data[128] = {'\0', };
 
-	//n = ceil(L_len/h);
+	n = ceil(L_len/h_len);
 
 	if(n > (pow(2, r) - 1)){
 		printf("error !\n");
+	}
+
+	w = 1;
+	if(Kl_len){
+		w += Kl_len;
 	}
 
 	for(int j=0; j<Label_len; j++){
@@ -38,37 +40,48 @@ void kdf_sha3_hmac_fb(const unsigned int rate, const unsigned int capacity, cons
 		input_data[w++] = Context[j];
 	}
 
-	input_data[w++] = L_len_to_Hex % 256;
+	input_data[w++] = L_len_to_Hex/256;
+	input_data[w] = L_len_to_Hex % 256;
 
-	L_len_to_Hex /= 256;
+	for(int i=0; i<w; i++){
+		printf("%02x", input_data[i]);
+	}printf("\n");
 
-	input_data[w++] = L_len_to_Hex % 256;
+	for(int i=0; i<n; i++){
+		w = 0;
 
-	for(int i=0; i<3; i++){ // i<3 고쳐야함
+		for(int j=0; j<Kl_len; j++){
+			input_data[w++] = Kl[j];
+		}
+
 		if(Kl_len){
-			for(num=0; num<Kl_len - 1; num++){
-				input_data[num] = 0;
+			for(int j=0; j< r/8; j++){
+				input_data[w] = i + 1;
+				printf("%02x", tmp_input_data[num]);
 			}
-			input_data[num] = i + 1;
+			printf("\n");
 		}
 
 		for(int j=0; j<input_data_len; j++){
-			printf("%02x", input_data[j]);
+			printf("%02x", tmp_input_data[j]);
 		}printf("\n");
 
 		hmac_digest(capacity / 2, rate, capacity, Kl, Kl_len, input_data, input_data_len, Kl);
-		//drbg_sha3_hmac_print(capacity / 16, Kl);
+		for(int i=0; i<capacity / 16; i++){
+			printf("%02x", Kl[i]);
+		}printf("\n");
 
 		for(int j=0; j<capacity / 16; j++){
 			output[output_len++] = Kl[j];
 		}
 	}
-
+	for(int i=0; i<capacity / 8; i++){
+		printf("%02x", output[i]);
+	}printf("\n");
 }
 
 void kdf_sha3_hmac_ctr(const unsigned int rate, const unsigned int capacity, const unsigned char delimitedSuffix, BitSequence *Kl, unsigned int Kl_len, BitSequence *Label, const unsigned int Label_len, BitSequence *Context, unsigned int Context_len, unsigned int L_len, unsigned int h_len, const unsigned int r, FILE *outf){
 	unsigned int n = 0;
-	BitSequence result[128] = {'\0', }; //고쳐야함
 	BitSequence output[128];
 	int w = 0;
 	int num = 0;
@@ -77,7 +90,6 @@ void kdf_sha3_hmac_ctr(const unsigned int rate, const unsigned int capacity, con
 	int input_data_len = Label_len + Context_len + 4;
 	BitSequence input_data[128] = {'\0', };
 	BitSequence tmp_input_data[128] = {'\0', };
-
 
 	n = ceil(L_len/h_len);
 
